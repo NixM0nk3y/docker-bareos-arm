fdc0f98da6aa24580bef4bf9c4175368040aad11
Nick Gregory <github@openenterprise.co.uk>
2019-07-10 19:53:29 +0000

    fixup extract script

--- extractdeb.sh
+++ extractdeb.sh
@@ -3,6 +3,8 @@
 DIRNAME=$(dirname "$0")
 
 containerID=$(docker run --detach nixm0nk3y/docker-bareos-arm:latest /bin/sleep 120)
-docker cp "$containerID:/tmp/bareos-Release-18.2.6/*.deb" "$DIRNAME"
+docker exec $containerID bash -c "mkdir /tmp/packages; mv /tmp/bareos-Release-*/*.deb /tmp/packages"
+docker cp "$containerID:/tmp/packages" .
 sleep 1
+docker kill "$containerID"
 docker rm "$containerID"
